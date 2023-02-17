03c7bdc40b2173be34f13a5722c4b4c4951514d5
Amit Saini <amitsaini703@gmail.com>
2023-02-17 18:29:41 +0100

    Initial Commit

---
+++ MVVMPractiseDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractiseDemo
+//
+//  Created by Amit on 17/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
